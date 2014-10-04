b0dade3caa872e35eb3f5572779aaf4160254994
Alkotronikk <Alkotronikk@users.noreply.github.com>
2014-10-04 23:31:25 +0200

    Create volume.sh

---
+++ .scripts/volume.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Working conky output for pulseaudio and i3bar with MUTE indicator.
+
+volume_muted=`amixer -D pulse | grep Playback | tail -1 | awk '{print $6}' | grep on`
+volume_perc=`amixer -D pulse | grep Playback | tail -1 | awk '{print $5}'`
+#echo $volume_muted $volume_perc
+
+if [ -z $volume_muted ]
+then
+echo "[MUTE]"
+else
+echo $volume_perc
