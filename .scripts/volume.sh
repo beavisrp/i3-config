de9c2acc4e176aac7ab07d9c5d6b3eb0f3c298ab
kounoale <kounoale@fit.cvut.cz>
2014-10-07 16:27:20 +0200

    Conditional formating of i3bar

--- .scripts/volume.sh
+++ .scripts/volume.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
 
-# Working conky output for pulseaudio and i3bar with MUTE indicator.
 
 volume_muted=`amixer -D pulse | grep Playback | tail -1 | awk '{print $6}' | grep on`
-volume_perc=`amixer -D pulse | grep Playback | tail -1 | awk '{print $5}'`
+#volume_perc=`amixer -D pulse | grep Playback | tail -1 | awk '{print $5}'`
 #echo $volume_muted $volume_perc
 
 if [ -z $volume_muted ]
 then
-echo "[MUTE]"
+echo 1
 else
-echo $volume_perc
+echo 0
 fi
--- .scripts/weather.sh
+++ .scripts/weather.sh
@@ -1,7 +1,5 @@
 #!/bin/sh
 
-# Using accuweather for temperature, change URL for your location.
-
 URL='http://www.accuweather.com/en/cz/prague/125594/weather-forecast/125594'
 
 wget -q -O- "$URL" | awk -F\' '/acm_RecentLocationsCarousel\.push/{print $12"°C" }'| head -1
