fba69303a3e557dcaac14051466886447a13e4a8
kounoale <kounoale@fit.cvut.cz>
2014-10-15 22:52:43 +0200

    15.10.2014 update

--- .scripts/weather.sh
+++ .scripts/weather.sh
@@ -2,4 +2,4 @@
 
 URL='http://www.accuweather.com/en/cz/prague/125594/weather-forecast/125594'
 
-wget -q -O- "$URL" | awk -F\' '/acm_RecentLocationsCarousel\.push/{print $12"°C" }'| head -1
+wget -q -O- "$URL" | awk -F\' '/acm_RecentLocationsCarousel\.push/{print $12"°C "$14 }'| head -1 | awk '{print toupper($0)}'
